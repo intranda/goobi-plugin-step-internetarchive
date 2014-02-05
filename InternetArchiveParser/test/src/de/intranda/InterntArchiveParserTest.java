9698e0da813b2265850e0da012eba1d483d3faf2
Robert Sehr <robert.sehr@intranda.com>
2014-02-05 10:51:27 +0100

    added build file, added first dummy test

---
+++ InternetArchiveParser/test/src/de/intranda/InterntArchiveParserTest.java
@@ -0,0 +1,14 @@
+package de.intranda;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InterntArchiveParserTest {
+
+    
+    @Test
+    public void testDummy() {
+        assertTrue(true);
+    }
+}
---
+++ InternetArchiveParser/test/src/de/intranda/TestAll.java
@@ -0,0 +1,29 @@
+/*************************************************************************
+ * 
+ * Copyright intranda GmbH
+ * 
+ * ************************* CONFIDENTIAL ********************************
+ * 
+ * [2003] - [2013] intranda GmbH, Bertha-von-Suttner-Str. 9, 37085 Göttingen, Germany 
+ * 
+ * All Rights Reserved.
+ * 
+ * NOTICE: All information contained herein is protected by copyright. 
+ * The source code contained herein is proprietary of intranda GmbH. 
+ * The dissemination, reproduction, distribution or modification of 
+ * this source code, without prior written permission from intranda GmbH, 
+ * is expressly forbidden and a violation of international copyright law.
+ * 
+ *************************************************************************/
+
+package de.intranda;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ })
+public class TestAll {
+
+}
